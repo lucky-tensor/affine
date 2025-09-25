63dd776ea362901f88623a7596643909b9e7af23
lucky-tensor <lucky-tensor>
2025-09-25 16:12:10 -0400

    update tests script

--- scripts/test.py
+++ scripts/test.py
@@ -18,7 +18,7 @@ def main():
     os.chdir(project_root)
 
     # Default to running all tests with verbose output
-    args = ["python", "-m", "pytest", "testsuite/", "-v"]
+    args = ["pytest", "testsuite/", "-v"]
 
     # Simple argument parsing
     if len(sys.argv) > 1:
